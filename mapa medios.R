4a397880adce721c7479b65e4b924b3438f7e75f
valeipr <valeria.peredorodriguez@gmail.com>
2023-11-22 18:01:31 -0400

    geojson cbba

--- mapa medios.R
+++ mapa medios.R
@@ -3,8 +3,11 @@ library(leaflet)
 
 datos <- read_csv("data/input/datos.csv")
 
-leaflet(datos) %>%
+mapa <- leaflet(datos) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addCircles(radius = ~sqrt(seguidores*500),
              popup = ~nombre,
+             fillColor = "transparent",
              weight = 3)
+
+save_html(mapa, file="output/mapa_preliminar.html")
